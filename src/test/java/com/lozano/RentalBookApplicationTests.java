6cd84cdd6798d851bfa9d550897bc41185421373
angeloml <angelo.ml.dev@gmail.com>
2025-06-11 20:47:08 -0500

    first commit

---
+++ src/test/java/com/lozano/RentalBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lozano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
